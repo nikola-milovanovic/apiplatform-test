7d89fff32fe7097f101f46f9823d6954fed91282
Nikola Milovanovic <nikola.milovanovic@clarivate.com>
2024-09-03 14:59:42 +0200

    add request headers in the body response

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -13,18 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
   @PostMapping("/acceptbody")
-  public ResponseEntity<String> acceptBody(@RequestHeader HttpHeaders headers, @RequestBody String body) {
+  public ResponseEntity<String> acceptBody(@RequestHeader HttpHeaders requestHeaders, @RequestBody String body) {
     // Log all headers
-    System.out.println("Headers:");
-    for (Map.Entry<String, String> entry : headers.toSingleValueMap().entrySet()) {
+    System.out.println("Request Headers:");
+    for (Map.Entry<String, String> entry : requestHeaders.toSingleValueMap().entrySet()) {
       System.out.println(entry.getKey() + ": " + entry.getValue());
     }
 
     // Log the body
     System.out.println("Body: " + body);
 
-    // Respond with a confirmation message
-    return new ResponseEntity<>("Headers and body received", HttpStatus.OK);
+    // Include request headers in the response body
+    StringBuilder responseBody = new StringBuilder("Headers and body received\n\nRequest Headers:\n");
+    for (Map.Entry<String, String> entry : requestHeaders.toSingleValueMap().entrySet()) {
+      responseBody.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+    }
+    responseBody.append("\nBody:\n").append(body);
+
+    // Respond with the headers and body
+    return new ResponseEntity<>(responseBody.toString(), HttpStatus.OK);
   }
 
 
